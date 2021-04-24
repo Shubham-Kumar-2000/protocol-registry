d6380ac2053af8b1e8437c7e2f869aad4d4ebf1f
san-saha <sanchitasaha31@gmail.com>
2021-04-24 14:46:34 +0530

    minor linux bug fixed

---
+++ temp/script.sh
@@ -0,0 +1,3 @@
+mv /home/san/Desktop/shubham/protocol-registry/temp/testproto.desktop  /usr/share/applications/
+cd /usr/share/applications/
+xdg-mime default testproto.desktop x-scheme-handler/testproto 
