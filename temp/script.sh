59213ec4c3e267c77fa647de80575f1404ef1435
san-saha <sanchitasaha31@gmail.com>
2021-04-24 14:50:00 +0530

    implemented tem script unlink

--- temp/script.sh
+++
@@ -1,3 +0,0 @@
-mv /home/san/Desktop/shubham/protocol-registry/temp/testproto.desktop  /usr/share/applications/
-cd /usr/share/applications/
-xdg-mime default testproto.desktop x-scheme-handler/testproto 
