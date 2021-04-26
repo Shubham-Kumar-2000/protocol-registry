cda0b0b036d6cd6b84bcb399a3238a9a4f869c30
Mayank <mayankku1307@gmail.com>
2021-04-26 22:19:52 +0530

    Updated main.m for mac

--- assets/mac/main.m
+++ assets/mac/main.m
@@ -1,31 +1,27 @@
 #import <Foundation/Foundation.h>
 #include <CoreServices/CoreServices.h>
+#include <stdio.h>
+#include<string.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         if(argc<2) return 1;
-        auto nullptr = NULL;
-        auto cfprotocol = CFStringCreateWithCString(
-        kCFAllocatorDefault, protocol, sizeof(protocol)/sizeof(protocol[0]));
-        if (cfprotocol == nullptr) {
+        CFStringRef cfprotocol = CFStringCreateWithCString(
+        kCFAllocatorDefault, argv[1], strlen(argv[1]));
+        if (cfprotocol == NULL) {
             return 1;
         }
-
         // Create a "fake" URL that only contains the protocol component of a URI.
-        auto url =
-            CFURLCreateWithString(kCFAllocatorDefault, cfprotocol, nullptr);
-        CFRelease(cfprotocol);
-        if (url == nullptr) {
+        CFURLRef url =CFURLCreateWithString(kCFAllocatorDefault, cfprotocol, NULL);
+        if (url == NULL) {
             return 1;
         }
 
         // List all application bundles that request this protocol scheme.
-        auto apps = LSCopyApplicationURLsForURL(url, kLSRolesAll);
-        if (apps == nullptr) {
+        CFArrayRef _Nullable apps = LSCopyApplicationURLsForURL(url, kLSRolesAll);
+        if (apps == NULL) {
             return 1;
         }
-
-        NSLog(@"Hello, World!");
     }
     return 0;
 }
