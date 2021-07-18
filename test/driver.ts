6d754ccfdec720b84cd1b4512f626df37aadb518
Aditya <adityaagupta31@gmail.com>
2021-07-19 01:17:37 +0530

    Ts file added

---
+++ test/driver.ts
@@ -0,0 +1,16 @@
+import { RegisterOptions } from "..";
+
+import path from "path";
+
+import ProtocolRegistry from "..";
+
+console.log("Registering...");
+ProtocolRegistry.register({
+  protocol: "testproto",
+  command: `node ${path.join(__dirname, "./tester.js")} $_URL_`,
+  override: true,
+  terminal: true,
+  script: true,
+}).then(async () => {
+  console.log("Successfully registered");
+});
