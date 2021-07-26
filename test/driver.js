b28b953174058d17a9d4661e39c3bec184dedbe0
Aditya <adityaagupta31@gmail.com>
2021-07-26 23:49:55 +0530

    linux postinstall timmer added

--- test/driver.js
+++ test/driver.js
@@ -4,7 +4,7 @@ const ProtocolRegistry = require('../src');
 
 console.log('Registering...');
 ProtocolRegistry.register({
-    protocol: 'testproto',
+    protocol: 'test',
     command: `node ${path.join(__dirname, './tester.js')} $_URL_`,
     override: true,
     terminal: true,
