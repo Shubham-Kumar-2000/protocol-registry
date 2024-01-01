2c2498139e4e5a24aed37383d2cd13b6e65543d9
Grayseon <grayseon@gmail.com>
2023-12-31 23:34:20 -0600

    Regex matches more

--- src/utils/replaceWithExactPath.js
+++ src/utils/replaceWithExactPath.js
@@ -2,15 +2,24 @@ const which = require('which');
 
 function replace(command) {
     return command
-        .split(/&&|;|\n/)
+        .split(
+            /(?<!\\)(?:[&|;]|&&|\|\||\n)(?=(?:(?:[^"\\]|\\.)|"(?:[^"\\]|\\.)*")*$)/
+        )
         .map((cmd) => {
-            cmd = cmd.trim().split(' ');
+            cmd = cmd
+                .trim()
+                .split(/\s(?=(?:(?:[^"\\]|\\.)|"(?:[^"\\]|\\.)*")*$)/);
+            if (cmd[0].length == 0) return;
+            console.log(cmd);
             try {
                 cmd[0] = which.sync(cmd[0]); // Replace the command to it's absolute path using which.
             } catch {
                 throw new Error(`Command doesn't exist: ${cmd[0]}`);
             }
             return cmd.join(' ');
+        })
+        .filter((e) => {
+            return e !== undefined;
         })
         .join(' && ');
 }
