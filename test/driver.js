31ebe0c6f83848693b6f899692ce264769cbea96
Shubham-Kumar-2000 <shukhu10@gmail.com>
2025-03-15 19:54:23 +0530

    driver fix

--- test/driver.js
+++ test/driver.js
@@ -3,18 +3,20 @@ const path = require('path');
 const ProtocolRegistry = require('../src');
 
 console.log('Registering...');
-ProtocolRegistry.register({
-    protocol: 'testproto',
-    command: `node '${path.join(__dirname, './tester.js')}' $_URL_`,
-    override: true,
-    terminal: true,
-    script: true,
-    scriptName: 'my-custom-script-name'
-}).then(async () => {
+ProtocolRegistry.register(
+    'testproto',
+    `node '${path.join(__dirname, './tester.js')}' $_URL_`,
+    {
+        override: true,
+        terminal: true,
+        script: true,
+        scriptName: 'my-custom-script-name'
+    }
+).then(async () => {
     console.log('Successfully registered');
 });
 
-ProtocolRegistry.checkifExists('testproto').then(console.log);
+ProtocolRegistry.checkIfExists('testproto').then(console.log);
 
 ProtocolRegistry.deRegister('testproto', {})
     .then(console.log)
--- test/driver.ts
+++ test/driver.ts
@@ -1,17 +1,16 @@
-import { RegisterOptions } from "..";
-
 import path from "path";
 
 import ProtocolRegistry from "..";
 
 console.log("Registering...");
-ProtocolRegistry.register({
-  protocol: "testproto",
-  command: `node ${path.join(__dirname, "./tester.js")} $_URL_`,
-  override: true,
-  terminal: true,
-}).then(async () => {
-  console.log("Successfully registered");
-});
+ProtocolRegistry.register(
+  "testproto",
+  `node ${path.join(__dirname, "./tester.js")} $_URL_`,
+  {
+    override: true,
+    terminal: true,
+  }).then(async () => {
+    console.log("Successfully registered");
+  });
 
 ProtocolRegistry.checkIfExists("testproto").then(console.log);
