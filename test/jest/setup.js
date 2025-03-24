6c326c2b4badaac5151401a621596201a59eb312
Shubham-Kumar-2000 <shukhu10@gmail.com>
2025-03-24 21:08:29 +0530

    Add Jest setup file and update configuration for integration testing

---
+++ test/jest/setup.js
@@ -0,0 +1,7 @@
+const constants = require('../config/constants');
+
+console.log(
+    'Jest Setup: Environment Constants ',
+    JSON.stringify(constants, null, 4),
+    '\n----------------------------------------------------'
+);
